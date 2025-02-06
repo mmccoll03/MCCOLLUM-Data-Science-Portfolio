f9f40421ae0ed0bddb330a359ad5da716bc37a9c
mmccoll03 <mattmccollum03@gmail.com>
2025-02-06 10:32:31 -0500

    Update Week_4_2_streamlit_data_IN-CLASS.py

--- basic-streamlit-app/Week_4_2_streamlit_data_IN-CLASS.py
+++ basic-streamlit-app/Week_4_2_streamlit_data_IN-CLASS.py
@@ -44,7 +44,7 @@ st.dataframe(filtered_df)
 # This teaches students how to work with external data in Streamlit
 # # Ensure the "data" folder exists with the CSV file
 # Display the imported dataset
-df2 = pd.read_csv("/Users/philipmccollum/Documents/GitHub/MCCOLLUM-Data-Science-Portfolio/basic-streamlit-app/data/sample_data.csv")
+df2 = pd.read_csv("data/sample_data.csv")
 st.dataframe(df2)
 # Using a selectbox to allow users to filter data by city
 # Students learn how to use widgets in Streamlit for interactivity
